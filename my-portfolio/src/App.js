9a18b222c275251129b94316a1c1fd8d27ee0e98
pdspds <pds4629@naver.com>
2020-04-26 22:29:26 +0900

    intro page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+      <h1>Intro Page</h1>
+        <div className="bubbles-container">
+        <svg className="bubbles" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 701 1024">
+
+            <g className="bubbles-large" strokeWidth="7">
+                <g>
+                    <g transform="translate(10 940)">
+                        <circle cx="35" cy="35" r="35"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(373 940)">
+                        <circle cx="35" cy="35" r="35"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(408 940)">
+                        <circle cx="35" cy="35" r="35"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(621 940)">
+                        <circle cx="35" cy="35" r="35"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(179 940)">
+                        <circle cx="35" cy="35" r="35"/>
+                    </g>
+                </g>
+            </g>
+
+            <g className="bubbles-small" strokeWidth="4">
+                <g>
+                    <g transform="translate(147 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(255 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(573 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(429 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(91 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(640 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(321 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(376 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(376 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+                <g>
+                    <g transform="translate(497 984)">
+                        <circle cx="15" cy="15" r="15"/>
+                    </g>
+                </g>
+            </g>
+
+        </svg>
+        </div>
+
+
+        </div>
     </div>
   );
 }
